1daadb80cec14a14ad7f231a1b9b287f75fc20ac
Evgeny Krysenkov <eugene.krysenkov@yandex.com>
2025-09-26 18:50:42 +0300

    add allure settings

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,13 +1,15 @@
 import allure
-import time
-
 from pages.login_page import LoginPage
 import data.variables
 import data.urls
 
 
+@allure.parent_suite("Автоматизация UI")
+@allure.suite("Примеры тестов UI")
+@allure.sub_suite("Основные проверки")
 class TestBasicFunctionality:
-
+    @allure.description(
+        'После аутентификации поиск текста "Dashboard" в личном кабинете')
     @allure.title('Успешный логин')
     def test_success_login(self, driver):
         page = LoginPage(driver)
@@ -18,6 +20,8 @@ class TestBasicFunctionality:
         expected = page.get_dashboard_title()
         assert data.variables.dashboard_text == expected
 
+    @allure.description(
+        'После ошибки аутентификации поиск текста "Invalid credentials" на странице логина')
     @allure.title('Неуспешный логин')
     def test_failure_login(self, driver):
         page = LoginPage(driver)
@@ -25,6 +29,5 @@ class TestBasicFunctionality:
         page.input_bad_name_text()
         page.input_bad_password_text()
         page.click_button_login()
-        time.sleep(3)
         expected = page.get_invalid_credentials()
         assert data.variables.invalid_credentials_text == expected
